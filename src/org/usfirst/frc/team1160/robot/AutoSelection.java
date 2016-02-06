a702d3c60e8e4d3c55917413c1861b19310a3c5c
z-ach <zachkaufman97@gmail.com>
2016-02-06 15:33:20 -0800

    autoselection stuff

--- src/org/usfirst/frc/team1160/robot/AutoSelection.java
+++ src/org/usfirst/frc/team1160/robot/AutoSelection.java
@@ -2,11 +2,27 @@ package org.usfirst.frc.team1160.robot;
 
 import org.usfirst.frc.team1160.robot.commands.auto.LowBar;
 
+import edu.wpi.first.wpilibj.Joystick;
 import edu.wpi.first.wpilibj.command.Command;
 
 public class AutoSelection {
 	
-	public AutoSelection(){
+	boolean[] on;
+	int[] flipped = {0,0};
+	
+	public AutoSelection(Joystick input){
+		on = new boolean[input.getButtonCount()];
+		
+		for(int count = 0; count < input.getButtonCount(); count++){
+			on[count] = input.getRawButton(count);
+			if(flipped[0] == 0 && on[count]){
+				flipped[0] = count;
+			}
+			else if(on[count]){
+				flipped[1] = count;
+				return;
+			}
+		}
 		
 	}
 	
